970ade66cde0e8c39657081a05a0bc87b341ec63
SefaliSabnam <sefali.sabnam26@gmail.com>
2025-03-20 17:09:35 +0530

    added all files-2

--- main.tf
+++ main.tf
@@ -48,22 +48,6 @@ resource "aws_s3_bucket_public_access_block" "static_website_block" {
   restrict_public_buckets = false
 }
 
-# ✅ Define the bucket policy to allow public read access
-resource "aws_s3_bucket_policy" "allow_public_read" {
-  bucket = aws_s3_bucket.static_website.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.static_website.arn}/*"
-      }
-    ]
-  })
-}
-
 # ✅ Upload index.html to the bucket
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.static_website.id
